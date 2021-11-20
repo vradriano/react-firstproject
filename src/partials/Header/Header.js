7a50a1534a58cdc0043f9d3ad3d91aac77ca6c58
Vitor Adriano <vradriano@hotmail.com>
2021-11-19 23:14:28 -0300

    adicionando estrutura inicial

---
+++ src/partials/Header/Header.js
@@ -0,0 +1,40 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from '@material-ui/core'
+
+import MenuIcon from '@material-ui/icons/Menu'
+import useStyles from '../Header/Header.style'
+
+
+const Header = () => {
+  const classes = useStyles()
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header
